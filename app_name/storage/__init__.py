2b625324cbd8179a79cbdaf289c99108352fa5b7
Tanawat Thuamthet <tanawat-th@nctc.cgn.nidec-copal.co.jp>
2024-02-08 08:36:11 +0700

    debug

--- app_name/storage/__init__.py
+++ app_name/storage/__init__.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions  
+from rest_framework import generics, permissions
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework.views import APIView, View
@@ -26,29 +26,35 @@ from rest_framework.views import exception_handler
 
 from rest_framework import serializers
 
+
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
+
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
+
     def post(self, request, *args, **kwargs):
         serializer = FileUploadSerializer(data=request.data)
         if serializer.is_valid():
             uploaded_file = serializer.validated_data['file']
             fs = FileSystemStorage()
-            token = str(random.randint(0,10))
-            token += str(random.randint(0,10))
-            token += str(random.randint(0,10))
+            token = str(random.randint(0, 10))
             token += str(random.randint(0, 10))
-            hashGenerate = hashlib.sha256(("FILES"+token).encode('utf-8')).hexdigest().upper()
-            filename = fs.save(hashGenerate + '.' + uploaded_file.name.split('.')[len(uploaded_file.name.split('.'))-1], uploaded_file)
+            token += str(random.randint(0, 10))
+            token += str(random.randint(0, 10))
+            hashGenerate = hashlib.sha256(
+                ("FILES"+token).encode('utf-8')).hexdigest().upper()
+            filename = fs.save(hashGenerate + '.' + uploaded_file.name.split('.')
+                               [len(uploaded_file.name.split('.'))-1], uploaded_file)
             file_url = fs.url(filename)
-            return Response({'data':str(file_url)},status=status.HTTP_200_OK)
+            print('flie upload:', filename, file_url)
+            return Response({'data': str(file_url)}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
     def get(self, request, *args, **kwargs):
         file_url = request.GET['image']
-        file = open(settings.MEDIA_ROOT+'\\'+file_url.replace('/','\\'), 'rb')
+        file = open(settings.MEDIA_ROOT+'\\'+file_url.replace('/', '\\'), 'rb')
         response = FileResponse(file)
         return response
